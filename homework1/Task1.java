cea81a85e1b18f4d9435cce413cc53d880c63790
Anton Yatsenko <yacenko372o@gmail.com>
2022-07-16 20:49:55 +0300

    First homework(main task + addition one)

--- homework1/Task1.java
+++ homework1/Task1.java
@@ -2,8 +2,17 @@ package homework1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println(3 + 3);
-        System.out.println(3 + 3);
+        System.out.println("****  ****      *      *   *");
+        System.out.println("*     *  *     * *     ** **");
+        System.out.println("**    ****    *   *    * * *");
+        System.out.println("*     *      *******   *   *");
+        System.out.println("****  *     *       *  *   *");
+        System.out.println("\n");
+        System.out.println("*   *  *****  *     *       **");
+        System.out.println("*   *  *      *     *      *  *");
+        System.out.println("*****  ****   *     *     *    *");
+        System.out.println("*   *  *      *     *      *  *");
+        System.out.println("*   *  *****  ****  ****    **");
+
     }
 }
