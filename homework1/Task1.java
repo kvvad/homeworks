eb740926c58392a7a28ba3c4571a7290f95e7e14
Anton Yatsenko <yacenko372o@gmail.com>
2022-07-29 12:07:45 +0300

    I did two assignments. The third one I don't quite understand how to do.

--- homework1/Task1.java
+++ homework1/Task1.java
@@ -3,33 +3,30 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("****  ****      *      *   *");
-        System.out.println("*     *  *     * *     ** **");
-        System.out.println("**    ****    *   *    * * *");
-        System.out.println("*     *      *******   *   *");
-        System.out.println("****  *     *       *  *   *\n");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Please, enter a symbol: ");
-        // Elements for strings to make a word HELLO from symbols
-        String s = sc.nextLine();
-        String sn = s + " ";
-        String ssp = " ";
-        String s2sp = "  ";
-        String s3 = s + s + s;
-        String s4 = s + s + s + s;
-        String she = sn + ssp + sn + s4 + ssp;
-        String s3sp = ssp + s2sp;
-        String s4sp = s2sp + s2sp;
-        // Ready strings
-        String str1 = she + sn + s3sp + sn + s3sp + s2sp + s + s;
-        String str2and4 = sn + ssp + sn + s + s4sp + sn + s3sp + sn + s4sp + sn + ssp + s;
-        String str3 = s4 + ssp + s3 + s2sp + sn + s3sp + sn + s3sp + sn + s3sp + s;
-        String str5 = she + s4 + ssp + s4 + ssp + s2sp + s + s;
 
-        System.out.println(str1);
-        System.out.println(str2and4);
-        System.out.println(str3);
-        System.out.println(str2and4);
-        System.out.println(str5);
+        // First task
+        byte number = 123;
+        int number2 = 321;
+        int result = number - number2;
+        System.out.println(result);
+
+        //Second task
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of puonds: ");
+        float pounds = sc.nextFloat();
+        double kilograms = pounds * 0.454;
+        System.out.println("It turns out " + kilograms + " kilograms");
+
+        //Third task
+        System.out.print("Enter the amount to be calculated: ");
+        int sum = sc.nextInt();
+        System.out.print("How many years: ");
+        int months = sc.nextInt();
+        System.out.print("How many percent: ");
+        int percent1 = sc.nextInt();
+        double percent = percent1 / 100;
+
     }
 }
